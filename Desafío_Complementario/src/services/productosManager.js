b82e6b7b93e810ba3a7ed62e8a5dac03343f3dc6
Mauro Nicolas Rivero <104471202+MauroNicolasRivero@users.noreply.github.com>
2023-11-30 21:29:00 -0300

    Update productosManager.js

--- Desafío_Complementario/src/services/productosManager.js
+++ Desafío_Complementario/src/services/productosManager.js
@@ -1,18 +1,10 @@
-/*
-En la carpeta servicios vamos a importar el modelo de esquema que construimos y el randomUUID
-de crypto para generar los id de forma automática y única
-Aqui crearemos la clase Manager que manejará los documentos a través de la lógica que vayamos
-creando, mormalmente acciones del tipo CRUD
-Vamos a necesitar exportar este manejador para la carpeta routes en el archivo de api... para
-que los endpoints puedan consumir los métodos aqui declarados
-*/
 
 import { dbProductos } from '../models/producto.mongoose.js'
 import { randomUUID } from 'crypto'
 
 class ProductosManager {
-    async consultar() {                                          // nombre el método creado por mi
-        return await dbProductos.find().lean()         // nombre del documento + metodos nativo de mongoose
+    async consultar() {                                         
+        return await dbProductos.find().lean()        
     }
 
     async consultarPorId(id) {
@@ -24,15 +16,15 @@ class ProductosManager {
     }
 
     async registrar(datosProductos) {
-        datosProductos._id = randomUUID()                               // _id creado de forma aleatoria por crypto
+        datosProductos._id = randomUUID()                               
         const producto = await dbProductos.create(datosProductos)
         return producto.toObject()
     }
 
     async actualizar(id,prodData) {
         const modificado = await dbProductos.findByIdAndUpdate(id,
-        { $set: prodData},                                         // $set método para manejar lo que trae el body
-        { new: true })                                   // metodo para indicarle que me traiga el nuevo resultado
+        { $set: prodData},                                       
+        { new: true })                                 
          .lean()
 
         if (!modificado) {
