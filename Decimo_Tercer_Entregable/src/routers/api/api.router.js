435b1a3f8ae543c06f57efe49d574202bc78b0bf
Mauro Nicolas Rivero <104471202+MauroNicolasRivero@users.noreply.github.com>
2024-02-04 16:45:33 -0400

    Update api.router.js

--- Decimo_Tercer_Entregable/src/routers/api/api.router.js
+++ Decimo_Tercer_Entregable/src/routers/api/api.router.js
@@ -5,7 +5,6 @@ import { sessionsRouter } from './sessions.router.js'
 import { errorHandler } from '../../middlewares/errorHandler.js'
 import { cartsRouter } from "./carts.router.js";
 import { productsRouter } from "./products.router.js";
-import { ticketsRouter } from './tickets.router.js'
 import { mockProductsRouter } from './mock.products.router.js' 
 
 export const apiRouter = Router()
@@ -20,7 +19,6 @@ apiRouter.use('/sessions', sessionsRouter)
 
 apiRouter.use('/carts', cartsRouter)
 apiRouter.use('/products', productsRouter)
-apiRouter.use('/tickets', ticketsRouter)
 
 apiRouter.use('/mockingproducts', mockProductsRouter)
 
